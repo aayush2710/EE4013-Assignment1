6c3c19d56c1bedf3a21e6270842f6f58c6601336
aayush <aayush.goyal54@gmail.com>
2021-08-20 20:03:06 +0530

    update

--- codes/verification.py
+++ codes/verification.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import subprocess
 import shlex
+from scipy.optimize import curve_fit
+
+# define the true objective function
+def objective(x, a, b):
+	return a * x + b
+ 
+# load the dataset
 
 def recursive(n):
     if(n==0):
@@ -14,16 +21,24 @@ def recursive(n):
     recursive(int(2*n/5))
 
 Time_arr = []
-for i in range(1000):
+for i in range(0,1005,5):
     start = time.time()
     recursive(i)
     end = time.time()
     Time_arr.append(end-start)
-
-plt.plot(np.array(range(1000)), np.array(Time_arr))
+x = np.array(range(0,1005,5))
+y = np.array(Time_arr)
+popt, _ = curve_fit(objective, x, y)
+a, b = popt
+x_line = np.array(range(1000))
+y_line = objective(x_line, a, b)
+plt.plot(x, y,c = "red", label = "Execution Time")
+plt.plot(x_line, y_line, ls='--',lw=1.5, label = "Linear Fit", c = "black")
 plt.title("Verification of complexity")
 plt.xlabel("n")
 plt.ylabel("time")
+plt.legend()
+plt.grid()
 plt.savefig("plot.eps")
 plt.show()
 
